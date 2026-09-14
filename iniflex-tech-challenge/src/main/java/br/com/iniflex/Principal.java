160bfbc1b757a5b15ece118ef33560fead58adc0
janluiz <jan_2020@hotmail.com>
2026-09-14 18:09:47 -0300

    feat: inicializa projeto maven e cria entidades Pessoa e Funcionario

---
+++ iniflex-tech-challenge/src/main/java/br/com/iniflex/Principal.java
@@ -0,0 +1,13 @@
+package br.com.iniflex;
+
+/**
+ * Hello world!
+ *
+ */
+public class Principal
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ iniflex-tech-challenge/src/main/java/br/com/iniflex/model/Funcionario.java
@@ -0,0 +1,29 @@
+package br.com.iniflex.model;
+
+public class Funcionario extends Pessoa {
+
+  private String funcao;
+  private BigDecimal salario;
+
+public Funcionario(String nome, LocalDate dataNascimento, String funcao, BigDecimal salario) {
+    super(nome, dataNascimento);
+    this.funcao = funcao;
+    this.salario = salario;
+  }
+
+  public String getFuncao() {
+    return funcao;
+  }
+
+  public BigDecimal getSalario() {
+    return salario;
+  }
+
+  public void setFuncao(String funcao) {
+    this.funcao = funcao;
+  }
+
+  public void setSalario(BigDecimal salario) {
+    this.salario = salario;
+  }
+}
